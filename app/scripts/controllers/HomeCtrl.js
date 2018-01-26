a47a32d7ec76200777b93a19490f528857486417
Daniel Burnside <dburnside22@gmail.com>
2018-01-26 09:21:52 -0500

    hide completed and timed out task

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,6 +1,20 @@
 (function(){
   function HomeCtrl(Task) {
-    this.tasks = Task.all;
+    this.activeTask = Task.activeTask;
+    this.task = Task;
+
+    this.hideTask = function(task){
+      var today = new Date();
+      var taskDate = new Date(task.createdAt);
+      console.log(today.getTime() > (taskDate.getTime()+ 6.048e+8));
+
+      if (today.getTime() > (taskDate.getTime()+ 6.048e+8)){
+        this.task.moveTask(task, "timedOut");
+      }
+      if (task.complete === true){
+        this.task.moveTask(task, "completedTask");
+      }
+    }
 
   }
 
--- app/scripts/services/Task.js
+++ app/scripts/services/Task.js
@@ -1,12 +1,31 @@
 (function() {
   function Task($firebaseArray){
     var Task = {};
-    var ref = firebase.database().ref().child("tasks");
-    var tasks = $firebaseArray(ref);
+    var ref = firebase.database().ref().child("activeTask");
+    var activeTask = $firebaseArray(ref);
+    var completedTaskRef = firebase.database().ref().child("completedTask");
+    var completedTask = $firebaseArray(completedTaskRef);
+    var timedOutRef = firebase.database().ref().child("timedOut");
+    var timedOut = $firebaseArray(timedOutRef);
 
-    Task.all = tasks;
+    Task.activeTask = activeTask;
 
+    Task.completedTask = completedTask;
 
+    Task.completeTask = function(task){
+      firebase.database().ref("activeTask/" + task.$id).update({
+        complete :true
+      });
+    }
+
+    Task.moveTask = function(task, where){
+      if (where === "timedOut"){
+        timedOut.$add(task);
+      } else if (where === "completedTask"){
+        completedTask.$add(task);
+      }
+      activeTask.$remove(task);
+    }
     return Task;
   }
 
